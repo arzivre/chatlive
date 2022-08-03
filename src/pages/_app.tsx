4b68e5a2168bbc641f63df9815c7b45f985999a4
Arzivre <sonyfauzi@outlook.co.id>
2022-08-03 17:30:07 +0700

    dev env working

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,47 +1,43 @@
-// src/pages/_app.tsx
 import { httpBatchLink } from '@trpc/client/links/httpBatchLink'
-import { createWSClient, wsLink } from '@trpc/client/links/wsLink'
+import { loggerLink } from '@trpc/client/links/loggerLink'
+import { wsLink, createWSClient } from '@trpc/client/links/wsLink'
 import { withTRPC } from '@trpc/next'
-import { SessionProvider } from 'next-auth/react'
-import type { AppType } from 'next/dist/shared/lib/utils'
-import type { AppRouter } from 'src/server/router'
-import 'src/styles/globals.css'
+import { getSession, SessionProvider } from 'next-auth/react'
+import getConfig from 'next/config'
+import { AppType } from 'next/dist/shared/lib/utils'
+import { AppRouter } from 'server/routers/_app'
 import superjson from 'superjson'
+import '../styles/globals.css'
 
-const MyApp: AppType = ({
-  Component,
-  pageProps: { session, ...pageProps },
-}) => {
+const { publicRuntimeConfig } = getConfig()
+
+const { NEXT_PUBLIC_APP_URL, NEXT_PUBLIC_WS_URL } = publicRuntimeConfig
+
+const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <SessionProvider session={session}>
+    <SessionProvider session={pageProps.session}>
       <Component {...pageProps} />
     </SessionProvider>
   )
 }
 
-const getBaseUrl = () => {
-  if (typeof window !== 'undefined') {
-    return ''
+MyApp.getInitialProps = async ({ ctx }) => {
+  return {
+    pageProps: {
+      session: await getSession(ctx),
+    },
   }
-  if (process.browser) return '' // Browser should use current path
-  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}` // SSR should use vercel url
-
-  return `http://localhost:${process.env.PORT ?? 3000}` // dev SSR should use localhost
 }
-
-const url = `${getBaseUrl()}/api/trpc`
 
 function getEndingLink() {
   if (typeof window === 'undefined') {
     return httpBatchLink({
-      url,
+      url: `${NEXT_PUBLIC_APP_URL}/api/trpc`,
     })
   }
-
   const client = createWSClient({
-    url: process.env.NEXT_PUBLIC_WS_URL || 'ws://localhost:3001',
+    url: NEXT_PUBLIC_WS_URL || 'ws://localhost:3001',
   })
-
   return wsLink<AppRouter>({
     client,
   })
@@ -55,19 +51,33 @@ export default withTRPC<AppRouter>({
      */
 
     return {
+      /**
+       * @link https://trpc.io/docs/links
+       */
       links: [
-        // pass logger link in here
+        // adds pretty logs to your console in development and logs errors in production
+        loggerLink({
+          enabled: (opts) =>
+            (process.env.NODE_ENV === 'development' &&
+              typeof window !== 'undefined') ||
+            (opts.direction === 'down' && opts.result instanceof Error),
+        }),
         getEndingLink(),
       ],
+      /**
+       * @link https://trpc.io/docs/data-transformers
+       */
       transformer: superjson,
       /**
        * @link https://react-query.tanstack.com/reference/QueryClient
        */
-      // queryClientConfig: { defaultOptions: { queries: { staleTime: 60 } } },
+      queryClientConfig: { defaultOptions: { queries: { staleTime: 60 } } },
       headers: () => {
         if (ctx?.req) {
+          // on ssr, forward client's headers to the server
           return {
             ...ctx.req.headers,
+            'x-ssr': '1',
           }
         }
         return {}
@@ -77,5 +87,5 @@ export default withTRPC<AppRouter>({
   /**
    * @link https://trpc.io/docs/ssr
    */
-  ssr: false,
+  ssr: true,
 })(MyApp)
--- src/pages/rooms/[roomId].tsx
+++ src/pages/rooms/[roomId].tsx
@@ -1,21 +1,117 @@
-import Head from 'next/head'
+import { Session } from 'next-auth'
+import { signIn, useSession } from 'next-auth/react'
 import { useRouter } from 'next/router'
+import { useState } from 'react'
+import { Message } from '../../constants/schemas'
+import { trpc } from '../../utils/trpc'
 
-const RoomId = () => {
-  const { query } = useRouter()
-  const roomId = query.roomId as string
+function MessageItem({
+  message,
+  session,
+}: {
+  message: Message
+  session: Session
+}) {
+  const baseStyles =
+    'mb-4 text-md w-7/12 p-4 text-gray-700 border border-gray-700 rounded-md'
+
+  const liStyles =
+    message.sender.name === session.user?.name
+      ? baseStyles
+      : baseStyles.concat(' self-end bg-gray-700 text-white')
+
   return (
-    <>
-      <Head>
-        <title>Chat Room {roomId}</title>
-      </Head>
-      <main className='container mxuauto flex flex-col items-center justify-center h-screen p-4'>
-        <h1 className='text-5xl md:text-[5rem] leading-normal font-extrabold text-gray-700'>
-          Welcome to Room {roomId}
-        </h1>
-      </main>
-    </>
+    <li className={liStyles}>
+      <div className='flex'>
+        <time>
+          {message.sentAt.toLocaleTimeString('en-AU', {
+            timeStyle: 'short',
+          })}{' '}
+          - {message.sender.name}
+        </time>
+      </div>
+      {message.message}
+      tes
+    </li>
   )
 }
 
-export default RoomId
+function RoomPage() {
+  const { query } = useRouter()
+  const roomId = query.roomId as string
+  const { data: session } = useSession()
+
+  const [message, setMessage] = useState('')
+  const [messages, setMessages] = useState<Message[]>([])
+
+  const { mutateAsync: sendMessageMutation } = trpc.useMutation([
+    'room.send-message',
+  ])
+
+  trpc.useSubscription(
+    [
+      'room.onSendMessage',
+      {
+        roomId,
+      },
+    ],
+    {
+      onNext: (message) => {
+        console.log('message', message)
+        setMessages((m) => {
+          return [...m, message]
+        })
+      },
+    }
+  )
+
+  if (!session) {
+    return (
+      <div>
+        <button onClick={() => signIn()}>Login</button>
+      </div>
+    )
+  }
+
+  return (
+    <div className='flex flex-col h-screen'>
+      <div className='flex-1'>
+        {JSON.stringify(session)}
+        <ul className='flex flex-col p-4'>
+          {messages.map((m) => {
+            return <MessageItem key={m.id} message={m} session={session} />
+          })}
+        </ul>
+      </div>
+
+      <form
+        className='flex'
+        onSubmit={(e) => {
+          console.log('submity')
+          e.preventDefault()
+
+          sendMessageMutation({
+            roomId,
+            message,
+          })
+          console.log(roomId, message)
+
+          setMessage('')
+        }}
+      >
+        <textarea
+          className='black p-2.5 w-full text-gray-700 bg-gray-50 rounded-md border border-gray-700'
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          placeholder='What do you want to say'
+        />
+
+        <button className='flex-1 text-white bg-gray-900 p-2.5' type='submit'>
+          Send message
+        </button>
+      </form>
+    </div>
+  )
+}
+
+export default RoomPage
